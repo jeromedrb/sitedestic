c6f9d9b309bfaf9c714af4081766f5edd08008cc
jeromedrb <jeromedrb@gmail.com>
2023-03-07 17:08:27 +0000

    admin base de donnée

--- sticweb/admin.py
+++ sticweb/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Message, Usersinfo, Solutions
+from .models import Usersinfo, demandesolution
 # Register your models here.
+
+
+
+admin.site.register(Message)
+admin.site.register(Usersinfo)
+admin.site.register(Solutions)
+admin.site.register(demandesolution)
